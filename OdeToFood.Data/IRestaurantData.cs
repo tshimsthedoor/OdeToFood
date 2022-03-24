b57d731997cfcb0ada6caed843dd0c373c7ccae9
Tresor Tshimanga <tshims79@gmail.com>
2022-03-24 20:03:23 +0200

    Added Page
    - I added the Details page
    - added the notfound page

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,68 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantByName(string name);
+        Restaurant GetById(int id);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant
+                {
+                    Id = 1,
+                    Name = "Scott's Pizza",
+                    Location = "Maryland",
+                    Cuisine = CuisineType.Indian
+                },
+                 new Restaurant
+                {
+                    Id = 2,
+                    Name = "Warehouse",
+                    Location = "Constantia",
+                    Cuisine = CuisineType.Mexican
+                },
+                  new Restaurant
+                {
+                    Id = 3,
+                    Name = "Panarotis Pizza",
+                    Location = "Claremont",
+                    Cuisine = CuisineType.Mexican
+                },
+                   new Restaurant
+                {
+                    Id = 4,
+                    Name = "FLM",
+                    Location = "Cape Town",
+                    Cuisine = CuisineType.Italian
+                }
+            };
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantByName(string name= null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+
+        }
+    }
+}
--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,22 +5,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration configuration;
+        private readonly IRestaurantData restaurantData;
 
         public string Message { get; set; }
+        public IEnumerable<Restaurant>  Restaurants { get; set; }
 
-        public ListModel(IConfiguration configuration)
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+
+        public ListModel(IConfiguration configuration, IRestaurantData restaurantData)
         {
             this.configuration = configuration;
+            this.restaurantData = restaurantData;
         }
         public void OnGet()
         {
+            
             Message = configuration["Message"];
+            Restaurants = restaurantData.GetRestaurantByName(SearchTerm);
         }
     }
 }
